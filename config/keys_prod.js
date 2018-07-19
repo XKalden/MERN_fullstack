db9eff06a9806f5d4a101eb84f4bd2bd24c1f9c8
XKalden <kaldend.tsering@outlook.com>
2018-07-19 09:37:31 -0400

    update

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,5 @@
 module.exports = {
-    mongoURI: 'process.env.MONGO_URI',
-    jwtkey: 'process.env.SECRET_OF_KEY',
-};
-
-
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY
+  };
+  
